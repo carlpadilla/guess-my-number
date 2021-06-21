fa5ff2ba5b80081fec297f46082dceb5456546ab
carlpadilla <carlpadilla@protonmail.com>
2021-06-21 16:32:31 -0400

    adding event listener

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+//event listener
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number';
+  }
+});
